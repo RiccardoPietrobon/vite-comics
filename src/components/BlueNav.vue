c8acd342ef79281ba75eaa59d6066df12215449b
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-27 17:36:50 +0100

    header

---
+++ src/components/BlueNav.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "BlueNav",
+        };
+    },
+};
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use "../assets/scss/mymixin.scss";
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                "CHARACTERS",
+                "COMICS",
+                "MOVIES",
+                "TV",
+                "GAMES",
+                "COLLECTIBLES",
+                "VIDEOS",
+                "FANS",
+                "NEWS",
+                "SHOP",
+            ]
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <nav class="container">
+            <!-- logo dc -->
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="DC_logo">
+            </div>
+            <!-- items navbar -->
+            <Ul>
+                <li v-for="item in items">{{ item }}</li>
+            </Ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/mymixin.scss" as *;
+
+.container {
+    @include between;
+}
+
+ul {
+    @include flex-center;
+
+    li {
+        margin: 0 10px;
+        padding: 70px 0px;
+        font-weight: bold;
+        font-size: 12px;
+
+        &:hover {
+            color: hsl(209deg 98% 49%);
+            border-bottom: 4px solid hsl(209deg 98% 49%);
+            cursor: pointer;
+        }
+    }
+}
+
+.logo,
+ul {
+    width: 50%;
+}
+</style>
