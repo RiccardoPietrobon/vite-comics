bf108f39905c38ddb6810d604416969f1d07bed8
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-28 16:51:34 +0100

    titles ok

--- src/components/CardHero.vue
+++ src/components/CardHero.vue
@@ -1,13 +1,22 @@
 <script>
 export default {
-    data() {
-        return {
-        };
+    props: {
+        singleCard: Object,
     },
+    methods: {
+        getImagePath: function (img) {
+            return new URL(img, import.meta.url).href;
+        }
+    }
 };
 </script>
 
-<template></template>
+<template>
+    <li>
+        <img :src="singleCard.thumb" alt="">
+        <h2>{{ singleCard.series }}</h2>
+    </li>
+</template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/mymixin.scss" as *;
--- src/components/JumboCards.vue
+++ src/components/JumboCards.vue
@@ -1,14 +1,41 @@
 <script>
+
+import CardHero from './CardHero.vue';
+
 export default {
-    data() {
-        return {
-        };
+    props: {
+        arrayCards: Array,
     },
+
+    components: { CardHero },
 };
 </script>
 
-<template></template>
+<template>
+    <img src="../assets/img/jumbotron.jpg" alt="">
+    <section class="cards-section">
+        <div class="container">
+            <a href=""></a>
+            <ul>
+                <!-- ciclo e faccio passare singleCard ovvero l'oggetto con tutti i suoi parametri -->
+                <CardHero v-for="card in arrayCards" :singleCard="card" />
+            </ul>
+        </div>
+
+    </section>
+</template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/mymixin.scss" as *;
+
+img {
+    height: 400px;
+    width: 100%;
+    object-fit: cover;
+    object-position: top;
+}
+
+.cards-section {
+    background-color: $three-color;
+}
 </style>
